19fd7dfe3872a70dfca0f1a95b9f0aa13615d883
Will Harris <Harriswill22@yahoo.com>
2018-10-12 14:06:35 -0400

    adds pictures

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const IMAGES = 
+[
+    "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+    "https://solarsystem.nasa.gov/system/stellar_items/image_files/38_saturn_1600x900.jpg",
+    "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/05/02/11/jupiter-red-spot.jpg?w968h681",
+    "https://solarsystem.nasa.gov/system/feature_items/images/82_carousel_neptune_1.jpg"
+];
